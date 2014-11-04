861fdb47226db135d8532a7cde3297773b82de34
Kenneth Prado <Kennethprado45@gmail.com>
2014-11-04 12:03:41 -0800

    blog project fix the comments on navigation.php

--- view/navigation.php
+++ view/navigation.php
@@ -1,7 +1,8 @@
 <?php
+	// this require once connects to config.php
 	require_once(__DIR__ ."/../model/config.php");
 ?>
-	 <!-- navigation was made to connect config.php to post.php to the rest --> 
+	
 <nav>
 	<ul>
 		<li><a href="<?php echo $path . "post.php"?>">Blog Post Form</a></li>
