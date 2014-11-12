d98d248b025b4417e709cf1a1b2f92d25552e136
Kenneth Prado <Kennethprado45@gmail.com>
2014-11-12 12:23:17 -0800

    First Blog Project worked on create-post and form.php to the code

--- view/form.php
+++ view/form.php
@@ -1,13 +1,16 @@
+<?php
+	require_once(__DIR__ . "/../model/config.php");
+?>
 <h1>Create Blog Post</h1>
-
-<form>
+//added a form method
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">
 	<div>
-	<label for="title">Title: </label>
-	<input type="text" name="title" />
-		</div>
-		<div>
-	<label for="post">Post: </label>
-	<textarea name="post"></textarea>
+		<label for="title">Title: </label>
+		<input type="text" name="title" />
+	</div>
+	<div>
+		<label for="post">Post: </label>
+		<textarea name="post"></textarea>
 	</div>
 
 	<div>
