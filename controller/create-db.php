8344872c020053ee6a5d03c83a7148e6b778d7c6
Kenneth Prado <Kennethprado45@gmail.com>
2014-11-20 12:25:11 -0800

    First Blog Project Fixed on the errors for create-db and Database and going to work on fixing the config errors code

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,29 +1,6 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php");
 
-	
-
-	if($connection->connect_error) {
-		die("<p>Error: " . $connection->connect_error . "</p>");
-	}
-
-	$exists = $connection->select_db($database);
-	
-	if(!$exists) {
-		// Why should I use classes rather than just a collection of functions? well because fucntions are part of classes so thats why using classes is better.
-
-		// sends commands to the database
-		$query = $connection->query("CREATE DATABASE $database");
-		// checks if our query is successful
-		if($query) {
-			echo "<p>Successfully created database: " . $database . "</p>";
-		}
-	}
-	// 	shows that DATABASE exists 
-	else {
-		echo "<p>DATABASE already exists.</P>";
-	}
-
 	$query = $connection->query("CREATE TABLE posts ("
 		. "id int(11) NOT NULL AUTO_INCREMENT,"
 		. "title varchar (255) NOT NULL,"
@@ -37,5 +14,3 @@
 		echo "<p>$connection->error</p>";
 	}
 	// added paragraph tags to the code
-
-	$connection->close(); 
--- model/config.php
+++ model/config.php
@@ -1,4 +1,5 @@
 <?php
+	require_once(__DIR__ . "/database.php");
 	// added this as a config file
 	$path = "/pradok-blog/";
 
